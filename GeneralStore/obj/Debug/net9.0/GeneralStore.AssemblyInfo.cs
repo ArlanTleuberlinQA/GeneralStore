1a3deb031f0101503c7710d50c870c922146d9a0
ArlanTleuberlinQA <lovitopor68@gmail.com>
2025-06-25 18:18:40 +0300

    all clean

--- GeneralStore/obj/Debug/net9.0/GeneralStore.AssemblyInfo.cs
+++ GeneralStore/obj/Debug/net9.0/GeneralStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneralStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815f5bd11acac10e274fecca7805fdcd1d0d2cda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8378577abc8d73d7f7d9804f58789c498506498")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneralStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeneralStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
